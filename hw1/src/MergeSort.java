bc1d927a6ce7435dac2b27af2512d36fe97a0adf
DavidC <dcho6@hawk.iit.edu>
2020-02-13 07:44:00 -0600

    Got MergeSort to work with the test

--- hw1/src/MergeSort.java
+++ hw1/src/MergeSort.java
@@ -6,49 +6,75 @@ public class MergeSort {
 	// Divide function takes in an Array arr and an integer n, where arr is the array to be sorted
 	// and n is the size of the array that needs to be sorted.
 	// Breaks down the array to very small arrays.
-	// It is assumed that Array arr has no null values.
-	void mergeSort(int arr[], int n)
+	// It is assumed that Array arr has no null values., and that n is known
+	int[] mergeSort(int arr[], int n)
 	{
 		//First check to see if the array is the minimum size. If it is, return
-		if (n < 2) {
-			return;
+		if (n == 1) 
+		{
+			return arr;
 		}
-		
-		// Find the middle of the Array and split it into 2 sub-arrays: left and right
-		int middle = n / 2; //Find the midpoint of the array
-		int leftSize = middle;
-		int rightSize = n-middle;
-		int leftArr[] = new int[leftSize];
-		int rightArr[] = new int[rightSize];
-		
-		//fill the sub-arrays
-		int rightIndex = 0;
-		for (int x = 0; x < n; x++) {
-			if (x < middle) {
-				leftArr[x] = arr[x];
+		else 
+		{
+			// Find the middle of the Array and split it into 2 sub-arrays: left and right
+			int middle = n / 2; //Find the midpoint of the array
+			int leftSize = middle;
+			int rightSize = n-middle;
+			int leftArr[] = new int[leftSize];
+			int rightArr[] = new int[rightSize];
+			
+			//fill the sub-arrays
+			int rightIndex = 0;
+			for (int x = 0; x < n; x++) {
+				if (x < middle) {
+					leftArr[x] = arr[x];
+				}
+				
+				//After filling the left array, start filling the right array
+				else if (x >= middle) {
+					rightArr[rightIndex] = arr[x];
+					rightIndex++;
+				}
 			}
 			
-			//After filling the left array, start filling the right array
-			else if (x >= middle) {
-				rightArr[rightIndex] = arr[x];
-				rightIndex++;
-			}
+			// Call the recursive function break the array down until it is the smallest size. Store as sorted arrays
+			int sortedLeft[] = new int[leftSize];
+			int sortedRight[] = new int[rightSize];
+			sortedLeft = mergeSort(leftArr, leftSize);
+			sortedRight = mergeSort(rightArr, rightSize);
+			
+			//Finally, merge the two sub-arrays while sorting
+			return merge(sortedLeft, leftSize, sortedRight, rightSize);
 		}
-		
-		// Recurse to Recursively break the array down until it is small
-		mergeSort(leftArr, leftSize);
-		mergeSort(rightArr, rightSize);
-		
-		//Finally, merge the two sub-arrays while sorting
-		merge(arr, leftArr, leftSize, rightArr, rightSize);
 	}
 	
 	// Merge function merges two arrays
-	// Checks for sorting during merge.
-	void merge(int arr[], int leftArr[], int leftSize, int rightArr[], int rightSize)
+	// Checks for sorting during merge. Takes in the sub-arrays and the sub-array sizes.
+	int[] merge(int leftArr[], int leftSize, int rightArr[], int rightSize)
 	{
+		// create the array that will be returned. It is the combined size of both sub-arrays
+		int retArr[] = new int[leftSize + rightSize];
 		
+		//initialize the indices
+		int leftIndex = 0;
+		int rightIndex = 0;
 		
+		for (int retIndex = 0; retIndex < retArr.length; retIndex++) 
+		{
+			if (rightIndex > rightSize-1 || (leftIndex < leftSize && leftArr[leftIndex] < rightArr[rightIndex]))
+			{
+				retArr[retIndex] = leftArr[leftIndex];
+				leftIndex++;
+			}
+			else
+			{
+				retArr[retIndex] = rightArr[rightIndex];
+				rightIndex++;
+			}
+			
+		}
+		
+		return retArr;
 		
 	}
 	
